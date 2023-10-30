ef5923e116aba9ae3a1c02759716d106e8221ae8
Jason C Rodriguez <jasoncrodriguez@gmail.com>
2023-10-29 21:36:30 -0400

    add resize step, add freqs to undo values

--- chladni_plate_generator.py
+++ chladni_plate_generator.py
@@ -22,27 +22,40 @@ class MESH_OT_chladni_plate(bpy.types.Operator):
     bl_label = "Chladni Plate"
     bl_options = {'REGISTER', 'UNDO'}
 
+    frequency_a: bpy.props.FloatProperty(
+        name = "frequency_a",
+        description = "Frequency of component A",
+        default = random.uniform(1.5, 4.5),
+        min = 0,
+        soft_max = 5,
+    )
+    frequency_b: bpy.props.FloatProperty(
+        name = "frequency_b",
+        description = "Frequency of component B",
+        default = random.uniform(1.5,4.5),
+        min = 0,
+        soft_max = 5,
+    )
+    
     @classmethod
     def poll(cls, context):
-        return True
-        #return context.area.type == 'VIEW_3D'
+        return context.area.type == 'VIEW_3D'
 
     def execute(self, context):
-        
+
         bpy.ops.mesh.primitive_plane_add(enter_editmode=False, align='WORLD', location=(0, 0, 0), scale=(1, 1, 1))
+        bpy.ops.transform.resize(value=(1, 1, 1))
         bpy.ops.object.modifier_add(type='SUBSURF')
         bpy.context.object.modifiers["Subdivision"].subdivision_type = 'SIMPLE'
         bpy.context.object.modifiers["Subdivision"].levels = 6
+        
         bpy.ops.object.modifier_apply(modifier="Subdivision")
 
         # Define Chladni pattern parameters
         amplitude_a = 0.1
         amplitude_b = 0.1  # Adjust the amplitude of the Chladni pattern
         
-        frequency_a = random.uniform(1.5,4.5)  # Adjust the frequency of the Chladni pattern
-        frequency_b = random.uniform(1.5,4.5)  # Adjust the frequency of the Chladni pattern
-        
-        bpy.context.selected_objects[0].name = 'chladni_plate_{:.2f}_{:.2f}'.format(frequency_a, frequency_b)
+        bpy.context.selected_objects[0].name = 'chladni_plate_{:.2f}_{:.2f}'.format(self.frequency_a, self.frequency_b)
 
         mode = bpy.context.active_object.mode
         bpy.ops.object.mode_set(mode='OBJECT')
@@ -53,8 +66,8 @@ class MESH_OT_chladni_plate(bpy.types.Operator):
 
         for vert in selectedVerts:
             x, y, z = vert.co
-            vert.co.z += amplitude_a * math.sin(frequency_a * x * math.pi) * math.sin(frequency_b * y * math.pi)\
-                            + amplitude_b * math.sin(frequency_b * x * math.pi) * math.sin(frequency_a * y * math.pi)
+            vert.co.z += amplitude_a * math.sin(self.frequency_a * x * math.pi) * math.sin(self.frequency_b * y * math.pi)\
+                            + amplitude_b * math.sin(self.frequency_b * x * math.pi) * math.sin(self.frequency_a * y * math.pi)
 
         mode = bpy.context.active_object.mode
         bpy.ops.object.mode_set(mode='OBJECT')
